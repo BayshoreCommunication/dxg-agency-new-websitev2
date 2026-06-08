7ba84897b1616b98f9dc2eefaa72d9c0b0a5d898
sajolbd <shamim15-9760@diu.edu.bd>
2026-06-08 19:06:45 +0600

    robots.txt and sitemap.xml file added

--- app/difference/page.tsx
+++ app/difference/page.tsx
@@ -1,7 +1,14 @@
 import BusinessNextLevelSection from "components/difference/BusinessNextLevelSection";
 import LetsTalkSection from "components/shared/LetsTalkSection";
 import React from "react";
-
+export const metadata = {
+  title: "The DXG Difference | Event Production That Stands Out",
+  description:
+    "Discover the DXG difference in event production, audiovisual design, and live experiences. We create impactful, seamless events that engage audiences.",
+  alternates: {
+    canonical: "/difference",
+  },
+};
 const page = () => {
   return (
     <div>
