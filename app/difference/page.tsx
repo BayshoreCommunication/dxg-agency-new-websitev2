06117d976d0b7e3bbcd912ac7668d59d147b285b
sajolbd <shamim15-9760@diu.edu.bd>
2026-06-08 19:03:23 +0600

    robots.txt and sitemap.xml file added

---
+++ app/difference/page.tsx
@@ -0,0 +1,14 @@
+import BusinessNextLevelSection from "components/difference/BusinessNextLevelSection";
+import LetsTalkSection from "components/shared/LetsTalkSection";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <BusinessNextLevelSection />
+      <LetsTalkSection />
+    </div>
+  );
+};
+
+export default page;
